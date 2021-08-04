4940a4e32e53c82e4fab8940344fbf816e63c9cc
Kostia <72884674+doichev-kostia@users.noreply.github.com>
2021-08-04 16:09:28 +0300

    Protected routes (#15)
    * Created PrivateRoute
    
    * Created and tested PrivateRoute
    
    * Fixed tests
    
    * Fixed eslint issues
    
    * fixed travis ci issue
    
    * Deleted default App.jsx test

--- client/src/view/App.jsx
+++ client/src/view/App.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
 import './assets/styles/App.scss';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppRoutes />
     </div>
   );
 }
