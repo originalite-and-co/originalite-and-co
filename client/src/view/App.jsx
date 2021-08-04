4daf3bc8bb161f2c69d46bd1fefbce680aaa2951
doichev-kostia <kostynd2004@gmail.com>
2021-08-04 21:44:33 +0300

    Fixed typo and deleted unused import in App.jsx

--- client/src/view/App.jsx
+++ client/src/view/App.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
 import './assets/styles/App.scss';
-import {createCatalog} from "../api/server";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" />
   );
 }
 
