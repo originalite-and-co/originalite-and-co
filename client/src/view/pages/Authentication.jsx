aa30f215c17f0bf6f980d5f9d6dd8110ac29c8f8
doichev-kostia <kostynd2004@gmail.com>
2021-08-04 22:31:38 +0300

    Created project routing

---
+++ client/src/view/pages/Authentication.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Authentication.propTypes = {
+
+};
+
+function Authentication(props) {
+    return (
+        <div>
+            Login/Register
+        </div>
+    );
+}
+
+export default Authentication;
---
+++ client/src/view/pages/Member.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Member.propTypes = {
+
+};
+
+function Member(props) {
+    return (
+        <div>
+            Profile/purchase history/favourites
+        </div>
+    );
+}
+
+export default Member;
