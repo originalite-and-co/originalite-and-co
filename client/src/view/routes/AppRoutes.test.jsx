292a4a6fe8191ab1b4783870fafa10f33f2faf20
doichev-kostia <kostynd2004@gmail.com>
2021-08-04 23:19:56 +0300

    Covered AppRoutes.jsx with tests. Installed previous version (4) of history library(module) because ReactRouter didn't work with 5th one. Fixed PrivateRoute tests

---
+++ client/src/view/routes/AppRoutes.test.jsx
@@ -0,0 +1,170 @@
+import {createBrowserHistory} from "history";
+import {render} from "@testing-library/react";
+import {Router} from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+
+jest.mock("../pages/Authentication.jsx", () => () => <p>Authentication</p>);
+jest.mock("../pages/Cart.jsx", () => () => <p>Cart</p>);
+jest.mock("../pages/Catalog.jsx", () => () => <p>Catalog</p>);
+jest.mock("../pages/Checkout.jsx", () => () => <p>Checkout</p>);
+jest.mock("../pages/Home.jsx", () => () => <p>Home</p>);
+jest.mock("../pages/Member.jsx", () => () => <p>Member</p>);
+jest.mock("../pages/Page404.jsx", () => () => <p>404</p>);
+
+
+/**
+ * Regular expression is used to check if component includes written text.
+ * Flag "i" means that there is no difference between upper or lowercase.
+ *
+ * @example getByText(/test/i) - this func will look through rendered components and find one
+ * that includes test, Test, TEST ... content
+ *
+ * */
+describe("AppRoutes", () => {
+    const history = createBrowserHistory();
+
+    beforeEach(() => {
+        history.push("/");
+        sessionStorage.clear()
+    })
+
+    test("if it renders home page", () => {
+        const {getByText} = render(
+            <Router history={history}>
+                <AppRoutes/>
+            </Router>
+        );
+
+        expect(getByText(/home/i)).toBeInTheDocument();
+    });
+
+    test("if it renders authentication page when pathname is '/auth/login' ", () => {
+        const {queryByText} = render(
+            <Router history={history}>
+                <AppRoutes/>
+            </Router>
+        );
+
+        expect(queryByText(/authentication/i)).toBeNull();
+        history.push("/auth/login");
+        expect(queryByText(/authentication/i)).toBeInTheDocument();
+    });
+
+    test("if it renders authentication page when pathname is '/auth/register' ", () => {
+        const {queryByText} = render(
+            <Router history={history}>
+                <AppRoutes/>
+            </Router>
+        );
+
+        expect(queryByText(/authentication/i)).toBeNull();
+        history.push("/auth/register");
+        expect(queryByText(/authentication/i)).toBeInTheDocument();
+    });
+
+    test("if it renders cart page ", () => {
+        const {queryByText} = render(
+            <Router history={history}>
+                <AppRoutes/>
+            </Router>
+        );
+
+        expect(queryByText(/cart/i)).toBeNull();
+        history.push("/cart");
+        expect(queryByText(/cart/i)).toBeInTheDocument();
+    });
+
+    test("if it renders catalog page ", () => {
+        const {queryByText} = render(
+            <Router history={history}>
+                <AppRoutes/>
+            </Router>
+        );
+
+        expect(queryByText(/catalog/i)).toBeNull();
+        history.push("/catalog");
+        expect(queryByText(/catalog/i)).toBeInTheDocument();
+    });
+
+    test("if it doesn't render checkout page without token", () => {
+        const {queryByText} = render(
+            <Router history={history}>
+                <AppRoutes/>
+            </Router>
+        );
+
+        expect(sessionStorage.getItem("token")).toBeNull();
+        expect(queryByText(/authentication/i)).toBeNull();
+        expect(queryByText(/checkout/i)).toBeNull();
+        history.push("/checkout");
+        expect(history.location.pathname).toMatch(/login/);
+        expect(queryByText(/checkout/i)).toBeNull();
+        expect(queryByText(/authentication/i)).toBeInTheDocument();
+    });
+
+    test("if it renders checkout page with token", () => {
+        const tokenValue = "test"
+        sessionStorage.setItem("token", tokenValue);
+
+        const {queryByText} = render(
+            <Router history={history}>
+                <AppRoutes/>
+            </Router>
+        );
+
+        expect(sessionStorage.getItem("token")).toMatch(tokenValue);
+        expect(queryByText(/authentication/i)).toBeNull();
+        expect(queryByText(/checkout/i)).toBeNull();
+        history.push("/checkout");
+        expect(history.location.pathname).toMatch(/checkout/);
+        expect(queryByText(/checkout/i)).toBeInTheDocument();
+        expect(queryByText(/authentication/i)).toBeNull();
+    });
+
+    test("If it doesn't render member page without token", () => {
+        const {queryByText} = render(
+            <Router history={history}>
+                <AppRoutes/>
+            </Router>
+        );
+
+        expect(sessionStorage.getItem("token")).toBeNull();
+        expect(queryByText(/authentication/i)).toBeNull();
+        expect(queryByText(/member/i)).toBeNull();
+        history.push("/member");
+        expect(history.location.pathname).toMatch(/login/);
+        expect(queryByText(/member/i)).toBeNull();
+        expect(queryByText(/authentication/i)).toBeInTheDocument();
+    });
+
+    test("If it renders member page with token", () => {
+        const tokenValue = "test"
+        sessionStorage.setItem("token", tokenValue);
+
+        const {queryByText} = render(
+            <Router history={history}>
+                <AppRoutes/>
+            </Router>
+        );
+
+        expect(sessionStorage.getItem("token")).toMatch(tokenValue);
+        expect(queryByText(/authentication/i)).toBeNull();
+        expect(queryByText(/member/i)).toBeNull();
+        history.push("/member");
+        expect(history.location.pathname).toMatch(/member/);
+        expect(queryByText(/member/i)).toBeInTheDocument();
+        expect(queryByText(/authentication/i)).toBeNull();
+    });
+
+    test("if it renders Page404 when the pathname doesn't match any Route", () => {
+        const {queryByText} = render(
+            <Router history={history}>
+                <AppRoutes/>
+            </Router>
+        );
+
+        expect(queryByText(/404/i)).toBeNull();
+        history.push("/incorrect-route-at-all");
+        expect(queryByText(/404/i)).toBeInTheDocument();
+    });
+})
