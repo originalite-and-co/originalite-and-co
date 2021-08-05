e426be2f8bc120358c67abc09c0789a31b1a0eee
doichev-kostia <kostynd2004@gmail.com>
2021-08-05 12:12:56 +0300

    Wrote some tests for reducer. Created function that helps to render components that use Redux for better testing

---
+++ client/src/redux/features/cart/cart.test.js
@@ -0,0 +1,26 @@
+import reducer from "./reducers"
+
+const {cart: cartReducer} = reducer;
+
+/**
+ * action {
+ *     type: "@@INIT"
+ * } is an action that redux use to initialize the store
+ * */
+describe("cart reducer", () => {
+    const initAction = {
+        type: "@@INIT"
+    }
+    test("if it returns initial value ", () => {
+        const result = cartReducer(undefined, initAction);
+        //the initial value is spotted in cartReducer function
+        expect(result).toStrictEqual([])
+    });
+
+    test("if it returns state when action type is not declared", () => {
+        const state = "test";
+        const result = cartReducer(state, initAction);
+
+        expect(result).toMatch(state);
+    });
+})
