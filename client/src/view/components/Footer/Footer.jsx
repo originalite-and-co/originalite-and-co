7f262c979ce3f8a0d6da0efd7568afbb46a6b3a3
max suheckiy <suheckiymax@gmail.com>
2021-08-10 14:06:32 +0300

    added check for absolute links and added media to scss

--- client/src/view/components/Footer/Footer.jsx
+++ client/src/view/components/Footer/Footer.jsx
@@ -6,42 +6,50 @@ import {Link} from "react-router-dom";
 import FooterStyles from './Footer.module.scss'
 
 const Footer = () => {
-    const {getData} = footerLinksOperations;
-    const {footer} = footerLinksSelectors;
-    const dispatch = useDispatch();
-    const links = useSelector(footer);
+        const {getData} = footerLinksOperations;
+        const {footer} = footerLinksSelectors;
+        const dispatch = useDispatch();
+        const links = useSelector(footer);
 
-    useEffect(() => {
-        dispatch(getData())
-    }, [dispatch])
+        useEffect(() => {
+            dispatch(getData())
+        }, [dispatch])
 
-    const footerContent = links.map((item, index) => {
-        return (
-            <Grid xs={12} sm={4} item key={index}>
-                <Box textAlign="left" paddingTop={1.5} paddingBottom={2}>
-                    <Typography variant="h6" component="h6"
-                                className={FooterStyles.title}>{item.title}</Typography>
-                </Box>
-                {item.links.map((item, index) => {
-                    return (
-                        <Box textAlign="left" pb="12px">
-                            <Link to={item.url} className={FooterStyles.link} key={index}>{item.description}</Link>
-                        </Box>
-                    )
-                })}
-            </Grid>
-        )
-    })
+        const checkTypeLink = url => {
+            if (url.includes('https')) return true
+        }
 
-    return (
-        <footer className={FooterStyles.footer} data-testid="footer">
-            <Container>
-                <Grid container>
-                    {footerContent}
+        const footerContent = links.map((item, index) => {
+            return (
+                <Grid xs={12} sm={4} item key={index}>
+                    <Box textAlign="left" paddingTop={1.5} paddingBottom={2}>
+                        <Typography variant="h6" component="h6"
+                                    className={FooterStyles.title}>{item.title}</Typography>
+                    </Box>
+                    {item.links.map((item, index) => {
+                        return (
+                            <Box textAlign="left" pb="12px">
+                                {checkTypeLink(item.url) ?
+                                    <a href={item.url} className={FooterStyles.link} key={index}
+                                       target="_blank">{item.description}</a> :
+                                    <Link to={item.url} className={FooterStyles.link} key={index}>{item.description}</Link>}
+                            </Box>
+                        )
+                    })}
                 </Grid>
-            </Container>
-        </footer>
-    )
-};
+            )
+        })
+
+        return (
+            <footer className={FooterStyles.footer} data-testid="footer">
+                <Container>
+                    <Grid container>
+                        {footerContent}
+                    </Grid>
+                </Container>
+            </footer>
+        )
+    }
+;
 
 export default Footer;
