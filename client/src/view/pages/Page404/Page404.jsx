1b607184c7289fb9f33cbd0e3f781ac305cbbe5b
Tolubaria <85737416+Tolubaria@users.noreply.github.com>
2021-08-13 19:53:38 +0300

    init

--- client/src/view/pages/Page404/Page404.jsx
+++ client/src/view/pages/Page404/Page404.jsx
@@ -13,21 +13,28 @@ function Page404(props) {
     return (
         <>
             <div className={stiles.ContainerPages404}>
-                <div className={stiles.ContainerLeft}>
                     <div className={stiles.BackMenu}>
                         <h1>Возможно вы имели ввиду:</h1>
                             <ul>
-                                <li className={stiles.BackMenu__item}>Women</li>
-                                <li className={stiles.BackMenu__item}>Men</li>
-                                <li className={stiles.BackMenu__item}>Accesory</li>
+                                <li className={stiles.BackMenu__item}>
+                                Women:
+                        </li>
+                        <span>_______________</span>
+                                <li className={stiles.BackMenu__item}>
+                                    Men:
+                        </li>
+                        <span>_______________</span>
+                                <li className={stiles.BackMenu__item}>
+                                    Kids:
+                        </li>
+                        <span>_______________</span>
                             </ul>
                     </div>
                     <div className={stiles.Info}>
                         <p className={stiles.Info__404}>404</p>
                         <p className={stiles.Info__item}>Потеряла Насяльника</p>
-                        <b className={stiles.Info__item}>Строаница не найдена</b>
+                        <b className={stiles.Info__item__a}>Строаница не найдена</b>
                     </div>
-                </div>
                 <div className={stiles.img}>
                     <img className={stiles.img__item} src={img} />
                 </div>
