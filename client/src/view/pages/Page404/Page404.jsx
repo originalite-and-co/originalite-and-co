d1092c73889fbbfedc0ff270da79642f4f8ba845
Tolubaria <85737416+Tolubaria@users.noreply.github.com>
2021-08-13 11:32:00 +0300

    init

--- client/src/view/pages/Page404/Page404.jsx
+++ client/src/view/pages/Page404/Page404.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import './Page404.module.scss';
+import img from '../Page404/img/404imeges.jpeg';
+import { withStyles } from '@material-ui/core/styles';
+
 import PropTypes from 'prop-types';
 
 Page404.propTypes = {
@@ -6,9 +10,20 @@ Page404.propTypes = {
 
 function Page404(props) {
     return (
-        <div>
-            404
-        </div>
+        <>
+            <div className="ContainerPages404">
+                <h1>Возможно вы имели ввиду:</h1>
+                <ul>
+                    <li>Women</li>
+                    <li>Men</li>
+                    <li>Accesory</li>
+                </ul>
+                <p>404</p>
+                <p>Потеряла Насяльника</p>
+                <b>Строаница не найдена</b>
+                    <img className="" src={img} />
+            </div>
+        </>
     );
 }
 
