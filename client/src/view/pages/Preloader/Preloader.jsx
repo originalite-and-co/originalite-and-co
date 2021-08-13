0ccbf8ced6c4c2514abe4458ade7ccdaf2697cae
Tolubaria <85737416+Tolubaria@users.noreply.github.com>
2021-08-13 17:21:49 +0300

    init

--- client/src/view/pages/Preloader/Preloader.jsx
+++ client/src/view/pages/Preloader/Preloader.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactLoading from 'react-loading';
+
 import { withStyles } from '@material-ui/core/styles';
 import Style from './Preloader.module.scss';
 import PropTypes from 'prop-types';
+import { green } from '@material-ui/core/colors';
 
-Preloader.propTypes = {
-};
+// Preloader.propTypes = {
+// };
 
 function Preloader(props) {
+
+   const [data, setData] = useState([]);
+   const [Done, setDone] = useState(undefined);
+
+   useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts/1')
+         .then((response) => response.json())
+         .then((json) => {
+            setData(json);
+         });
+   }, [])
+
     return (
-        <>
-          <div className={Style.ContainerPreloader}>
-             <div class="lds-roller">
-                <div>rjrjrrjrjr</div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-             </div>
-         </div>
+       <>
+          <ReactLoading type={bars} color={green} height={'667'} width={'375'} />
         </>
     );
 }
