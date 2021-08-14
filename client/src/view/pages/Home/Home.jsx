ba81b93b4e9b6a08a17c8fc1178e589c7faa8c66
Tolubaria <85737416+Tolubaria@users.noreply.github.com>
2021-08-15 01:07:42 +0300

    init

--- client/src/view/pages/Home/Home.jsx
+++ client/src/view/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
+import Preloader from "../Preloader/Preloader";
 
 Home.propTypes = {
 
@@ -12,7 +13,7 @@ function Home(props) {
   return (
     <>
       <Header />
-     
+      <Preloader />
         <div>Home</div>
       <Footer/>
     </>
---
+++ client/src/view/pages/Preloader/Preloader.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import Lottie from "react-lottie";
+
+import * as location from './1055-world-locations.json'
+
+const defaultOptions = {
+   loop: true,
+   autoplay: true,
+   animationData: location.default,
+   rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+   }
+ };
+
+// Preloader.propTypes = {
+// };
+
+function Preloader(props) {
+
+   const [data, setData] = useState([]);
+   const [loading, setloading] = useState(undefined);
+   const [complited, setcomplited] = useState(undefined);
+
+   useEffect(() => {
+      fetch("")
+         .then((response) => response.json())
+         .then((json) => {
+            console.log(json);
+            setData(json);
+            setloading(true);
+
+            setTimeout(() => {
+               setcomplited(true);
+            }, 1000);
+         });
+   }, []);
+
+   return (
+      <>
+         {!completed ? (
+            
+            <Lottie options={defaultOptions}
+              height={400}
+              width={400}
+            />
+         ) : (
+               <h1>Your Data</h1>
+        )}
+      </>
+   );
+}
+
+export default Preloader;
