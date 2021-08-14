7d22c12bdc6b749136bbb70914b9e092f51a7ac1
Tolubaria <85737416+Tolubaria@users.noreply.github.com>
2021-08-15 01:44:01 +0300

    init

--- client/src/view/pages/Page404/Page404.jsx
+++ client/src/view/pages/Page404/Page404.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-import stiles from './Page.module.scss';
+import styles from './Page.module.scss';
 
 import img from '../Page404/img/404imeges.jpeg';
 
@@ -14,37 +14,37 @@ Page404.propTypes = {
 function Page404(props) {
     return (
         <>
-            <div className={stiles.ContainerPages404}>
-                    <div className={stiles.BackMenu}>
-                        <h1 className={stiles.BackMenu__title}>Возможно вы имели ввиду:</h1>
+            <div className={styles.ContainerPages404}>
+                    <div className={styles.BackMenu}>
+                        <h1 className={styles.BackMenu__title}>Возможно вы имели ввиду:</h1>
                             <ul>
-                                <li className={stiles.BackMenu__item}>
+                                <li className={styles.BackMenu__item}>
                                     <Link to="/Women">Women:</Link>
                                 </li>
-                                    <span className={stiles.strip}>
-                                        <span className={stiles.item}></span>
+                                    <span className={styles.strip}>
+                                        <span className={styles.item}></span>
                                     </span>
-                                <li className={stiles.BackMenu__item}>
+                                <li className={styles.BackMenu__item}>
                                     <Link to="/Men">Men:</Link>
                                 </li>
-                                    <span className={stiles.strip}>
-                                        <span className={stiles.item}></span>
+                                    <span className={styles.strip}>
+                                        <span className={styles.item}></span>
                                     </span>
-                                <li className={stiles.BackMenu__item}>
+                                <li className={styles.BackMenu__item}>
                                     <Link to="/Kids">Kids:</Link>
                                 </li>
-                                    <span className={stiles.strip}>
-                                        <span className={stiles.item}></span>
+                                    <span className={styles.strip}>
+                                        <span className={styles.item}></span>
                                     </span>
                             </ul>
                     </div>
-                    <div className={stiles.Info}>
-                        <p className={stiles.Info__404}>404</p>
-                        <p className={stiles.Info__item}>Потеряла Насяльника</p>
-                        <b className={stiles.Info__item__a}>Строаница не найдена</b>
+                    <div className={styles.Info}>
+                        <p className={styles.Info__404}>404</p>
+                        <p className={styles.Info__item}>Потеряла Насяльника</p>
+                        <b className={styles.Info__item__a}>Строаница не найдена</b>
                     </div>
-                <div className={stiles.img}>
-                    <img className={stiles.img__item} src={img} />
+                <div className={styles.img}>
+                    <img className={styles.img__item} src={img} />
                 </div>
             </div>
         </>
