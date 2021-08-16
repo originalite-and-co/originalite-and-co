1fb68232dc2e888cd5510d09c92755d2cdb64446
drewmeltpool <drewmelt132@gmail.com>
2021-08-16 20:38:30 +0300

    feature: create validation for sign in and sign up

--- client/src/view/pages/Member/Member.jsx
+++ client/src/view/pages/Member/Member.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from "../../components/Button/Button";
-import {useDispatch} from "react-redux";
-import {authorizationActions, authorizeOperations} from "../../../redux/features/authorization";
+import Button from '../../components/Button/Button';
+import { useDispatch } from 'react-redux';
+import {
+  authorizationActions,
+  authorizeOperations,
+} from '../../../redux/features/authorization';
 
-Member.propTypes = {
-
-};
-
+Member.propTypes = {};
 
 function Member(props) {
-    const dispatch = useDispatch()
-    const handleSignOut = () => {
-        dispatch(authorizeOperations.loggOutUser())
-    }
+  const dispatch = useDispatch();
+  const handleSignOut = () => {
+    dispatch(authorizeOperations.loggOutUser());
+  };
 
-    return (
-        <div>
-            Profile/purchase history/favourites
-            <Button onClick={handleSignOut} type="button" text="SIGN OUT" color="black"/>
-        </div>
-    );
+  return (
+    <div>
+      Profile/purchase history/favourites
+      <Button
+        onClick={handleSignOut}
+        type="button"
+        text="SIGN OUT"
+        color="black"
+      />
+    </div>
+  );
 }
 
 export default Member;
