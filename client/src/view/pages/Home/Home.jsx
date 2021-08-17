9fcc3be446e7d7a6e9013aeb14f7fccb0c457137
drewmeltpool <drewmelt132@gmail.com>
2021-08-18 01:29:52 +0300

    feature: add range slider to filter

--- client/src/view/pages/Home/Home.jsx
+++ client/src/view/pages/Home/Home.jsx
@@ -1,26 +1,24 @@
-import React, {useEffect, useState} from 'react';
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer"
-import CardList from "../../components/CardList/CardList.jsx";
-import ShopCategories from "../../components/ShopCategories/ShopCategories";
-import {Box} from "@material-ui/core";
-import PopularProductsCarousel from "../../components/PopularProductsCarousel/PopularProductsCarousel";
+import React, { useEffect, useState } from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import CardList from '../../components/CardList/CardList.jsx';
+import ShopCategories from '../../components/ShopCategories/ShopCategories';
+import { Box } from '@material-ui/core';
+import PopularProductsCarousel from '../../components/PopularProductsCarousel/PopularProductsCarousel';
 
-import classes from "./Home.module.scss"
-
-Home.propTypes = {};
+import classes from './Home.module.scss';
 
 function Home(props) {
-    return (
-        <Box>
-            <Header/>
-            <Box className={`${classes.content} wrapper`} component={"main"}>
-               <PopularProductsCarousel/>
-                <ShopCategories/>
-            </Box>
-            <Footer/>
-        </Box>
-    );
+  return (
+    <Box>
+      <Header />
+      <Box className={`${classes.content} wrapper`} component={'main'}>
+        <PopularProductsCarousel />
+        <ShopCategories />
+      </Box>
+      <Footer />
+    </Box>
+  );
 }
 
 export default Home;
