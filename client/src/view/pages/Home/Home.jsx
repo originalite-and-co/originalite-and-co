f86c6574af27fd521d780f5ac9b8a4098b8a8dc6
Kostia <72884674+doichev-kostia@users.noreply.github.com>
2021-08-18 11:37:40 +0300

    Custom theme provider (#44)
    * Created CustomThemeProvider HOC
    
    * Fixed comma bug

--- client/src/view/pages/Home/Home.jsx
+++ client/src/view/pages/Home/Home.jsx
@@ -1,28 +1,25 @@
-import React, {useEffect, useState} from 'react';
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer"
-import ShopCategories from "../../components/ShopCategories/ShopCategories";
-import {Box} from "@material-ui/core";
-import PopularProductsCarousel from "../../components/PopularProductsCarousel/PopularProductsCarousel";
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import ShopCategories from '../../components/ShopCategories/ShopCategories';
+import { Box } from '@material-ui/core';
+import PopularProductsCarousel from '../../components/PopularProductsCarousel/PopularProductsCarousel';
 
-import classes from "./Home.module.scss"
-import { catalogRequests } from '../../../api/server';
+import classes from './Home.module.scss';
 
 Home.propTypes = {};
 
 function Home(props) {
-  catalogRequests.retrieveCatalog()
-    .then(data => console.log(data));
-    return (
-        <Box>
-            <Header/>
-            <Box className={`${classes.content} wrapper`} component={"main"}>
-               <PopularProductsCarousel/>
-                <ShopCategories/>
-            </Box>
-            <Footer/>
-        </Box>
-    );
+  return (
+    <Box>
+      <Header />
+      <Box className={`${classes.content} wrapper`} component={'main'}>
+        <PopularProductsCarousel />
+        <ShopCategories />
+      </Box>
+      <Footer />
+    </Box>
+  );
 }
 
 export default Home;
