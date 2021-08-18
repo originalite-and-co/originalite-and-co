49d851091a324049669c6152abe4aa2c5a913dab
Tolubaria <85737416+Tolubaria@users.noreply.github.com>
2021-08-18 07:37:19 +0300

    Pages404 (#42)
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * added Header Footer
    
    Co-authored-by: doichev-kostia <kostynd2004@gmail.com>

--- client/src/view/pages/Page404/Page404.jsx
+++ client/src/view/pages/Page404/Page404.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import Header from '../../components/Header/Header';
 import styles from './Page.module.scss';
-
+import Footer from "../../components/Footer/Footer";
 import img from '../Page404/img/oops.jpg';
 
 
@@ -15,29 +15,7 @@ function Page404(props) {
     return (
         <>
             <div className={styles.ContainerPages404}>
-                    <div className={styles.BackMenu}>
-                        <h1 className={styles.BackMenu__title}>Maybe you meant:</h1>
-                            <ul>
-                                <li className={styles.BackMenu__item}>
-                                    <Link to="/Women">Women:</Link>
-                                </li>
-                                    <span className={styles.strip}>
-                                        <span className={styles.item}></span>
-                                    </span>
-                                <li className={styles.BackMenu__item}>
-                                    <Link to="/Men">Men:</Link>
-                                </li>
-                                    <span className={styles.strip}>
-                                        <span className={styles.item}></span>
-                                    </span>
-                                <li className={styles.BackMenu__item}>
-                                    <Link to="/Kids">Kids:</Link>
-                                </li>
-                                    <span className={styles.strip}>
-                                        <span className={styles.item}></span>
-                                    </span>
-                            </ul>
-                    </div>
+                <Header />
                     <div className={styles.Info}>
                         <p className={styles.Info__404}>404</p>
                         <p className={styles.Info__item}>Page not found</p>
@@ -46,6 +24,7 @@ function Page404(props) {
                     <img className={styles.img__item} src={img} />
                 </div>
             </div>
+            <Footer />
         </>
     );
 }
