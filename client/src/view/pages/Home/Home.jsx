5f00cd119dc746a39805888756b767fd79171b57
doichev-kostia <kostynd2004@gmail.com>
2021-08-18 11:29:55 +0300

    Fixed comma bug

--- client/src/view/pages/Home/Home.jsx
+++ client/src/view/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, from 'react';
+import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import ShopCategories from '../../components/ShopCategories/ShopCategories';
