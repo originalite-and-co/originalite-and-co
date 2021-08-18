1b96787f2833f6b6b82ed6e2e75d464f2027c8b1
drewmeltpool <43030846+drewmeltpool@users.noreply.github.com>
2021-08-19 00:19:23 +0300

    Feature/carousel (#40)
    * refactor: carousel component
    
    * refactor: add style for promoCarousel
    
    * feature: create image component and some change in Home page
    
    * refactor: update Carousel component and Home page
    
    * refactor: update styles
    
    * Fixes after merge
    
    Co-authored-by: doichev-kostia <kostynd2004@gmail.com>

---
+++ client/src/view/components/Image/Image.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+Image.propTypes = {
+  src: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+function Image({ src, className }) {
+  const alt = src.split('/').pop().split('.')[0];
+  return <img src={src} className={className} alt={alt} />;
+}
+
+export default Image;
