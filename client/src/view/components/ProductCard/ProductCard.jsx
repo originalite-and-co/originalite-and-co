7f9b651a92e28b4e67cb7a0ed6e680e5804c4bd3
doichev-kostia <kostynd2004@gmail.com>
2021-08-20 09:53:27 +0300

    Change ProductCard title color depending on url

--- client/src/view/components/ProductCard/ProductCard.jsx
+++ client/src/view/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import styles from './ProductCard.module.scss';
 import { Grid, Typography, Box } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import useWindowSize from '../../hooks/useWindowSize';
 import constants from '../../constants';
@@ -15,6 +15,7 @@ function ProductCard({ product, size }) {
   const [isDesktop, setDesktop] = useState(false);
 
   const { width } = useWindowSize();
+  const { path } = useRouteMatch();
 
   useEffect(() => {
     setDesktop(width >= constants.WINDOW_DESKTOP_SIZE);
@@ -27,7 +28,7 @@ function ProductCard({ product, size }) {
           <img src={product.imageUrls[0]} alt='products images' />
         </div>
         <Typography
-          color='primary'
+          color={path === "/" ? "textPrimary" : 'textSecondary'}
           component='p'
           variant={isDesktop ? 'h6' : 'body2'}
           className={styles.productCardTitle}
@@ -44,7 +45,7 @@ function ProductCard({ product, size }) {
       </Link>
     </Box>
   );
-};
+}
 
 
 export default ProductCard;
