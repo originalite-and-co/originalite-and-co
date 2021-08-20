d752d26f0e256fe55c2d2452f4d4047172b5ae9a
doichev-kostia <kostynd2004@gmail.com>
2021-08-20 09:39:16 +0300

    Added styles for tablet and desktop versions

--- client/src/view/pages/Home/ShopCategories/styles.js
+++ client/src/view/pages/Home/ShopCategories/styles.js
@@ -1,4 +1,4 @@
-const generateStyles = (theme) => ({
+const generateStyles = ({ breakpoints }) => ({
   root: {},
   heading: {
     fontFamily: 'Open Sans, sans-serif',
@@ -14,6 +14,18 @@ const generateStyles = (theme) => ({
 "men women kids"
 "outwear outwear outwear"
 `,
+    [breakpoints.up("tablet")]: {
+      gridTemplateRows: '24vh 26vh',
+    },
+
+    [breakpoints.up("minDesktop")]: {
+      gridTemplateRows: '30vh 32vh',
+    },
+
+    [breakpoints.up("desktop")]: {
+      gridTemplateRows: '46vh 45vh',
+    }
+
   },
 });
 
