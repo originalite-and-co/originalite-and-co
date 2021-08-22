01d6c1b6c2e6a76abc8da99c22fbaef9e813628f
doichev-kostia <kostynd2004@gmail.com>
2021-08-22 13:24:52 +0300

    Fixed AppRoutes test

--- client/src/view/routes/AppRoutes.jsx
+++ client/src/view/routes/AppRoutes.jsx
@@ -1,81 +1,80 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
-import Home from "../pages/Home/Home";
-import Authentication from "../pages/Authentication/Authentication"
-import Page404 from "../pages/Page404/Page404";
+import Home from '../pages/Home/Home';
+import Authentication from '../pages/Authentication/Authentication';
+import Page404 from '../pages/Page404/Page404';
 
-import PrivateRoute from "./PrivateRoute/PrivateRoute";
-import Member from "../pages/Member/Member";
-import Checkout from "../pages/Checkout/Checkout";
-import Catalog from "../pages/Catalog/Catalog";
-import Cart from "../pages/Cart/Cart";
-import Product from "../pages/Product/Product";
-import SearchResult from "../pages/SearchResult/SearchResult";
+import PrivateRoute from './PrivateRoute/PrivateRoute';
+import Member from '../pages/Member/Member';
+import Checkout from '../pages/Checkout/Checkout';
+import Catalog from '../pages/Catalog/Catalog';
+import Cart from '../pages/Cart/Cart';
+import SearchResult from '../pages/SearchResult/SearchResult';
+import Product from '../pages/Product/Product';
 
-import useAsyncError from "../hooks/useAsyncError";
-import {linkRequests, pageRequests} from "../../api/server";
-import StaticPage from "../components/StaticPage/StaticPage";
-import {authorizationSelectors, authorizeOperations} from "../../redux/features/authorization";
-import {useDispatch, useSelector} from "react-redux";
-import ProductPage from "../pages/ProductPage/ProductPage";
+import useAsyncError from '../hooks/useAsyncError';
+import { linkRequests, pageRequests } from '../../api/server';
+import StaticPage from '../components/StaticPage/StaticPage';
+import { authorizationSelectors, authorizeOperations } from '../../redux/features/authorization';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function AppRoutes() {
-    const dispatch = useDispatch()
-    const authorization = useSelector(authorizationSelectors.authorization)
-    const [isAuthenticated, setAuthenticated] = useState(!!sessionStorage.getItem('token') || !!localStorage.getItem("token"));
+  const dispatch = useDispatch();
+  const authorization = useSelector(authorizationSelectors.authorization);
+  const [isAuthenticated, setAuthenticated] = useState(!!sessionStorage.getItem('token') || !!localStorage.getItem('token'));
 
-    const [staticPages, setStaticPages] = useState([]);
+  const [staticPages, setStaticPages] = useState([]);
 
-    const throwError = useAsyncError();
-
-
-    useEffect(() => {
-        dispatch(authorizeOperations.authorizeUser())
-        setAuthenticated(!!sessionStorage.getItem('token') || !!localStorage.getItem("token") );
-    }, [authorization,isAuthenticated]);
+  const throwError = useAsyncError();
 
 
-    useEffect(useCallback(() => {
-        pageRequests.retrievePages()
-            .then(
-                data => setStaticPages(data),
-                error => throwError(error)
-            );
-    }, [staticPages]), []);
+  useEffect(() => {
+    dispatch(authorizeOperations.authorizeUser());
+    setAuthenticated(!!sessionStorage.getItem('token') || !!localStorage.getItem('token'));
+  }, [authorization, isAuthenticated]);
 
-    let staticPageRoutes;
 
-    if (staticPages.length){
-       staticPageRoutes = staticPages.map(page => {
-           return <Route
-               key={page._id}
-               path={page.url}
-               render={(renderProps) =>
-                   <StaticPage
-                       title={page.title}
-                       htmlContent={page.htmlContent}
-                       {...renderProps}/>}/>
-       })
-    }
+  useEffect(useCallback(() => {
+    pageRequests.retrievePages()
+      .then(
+        data => setStaticPages(data),
+        error => throwError(error),
+      );
+  }, [staticPages]), []);
 
-    return (
-        <Switch>
-            <Route path="/products/search" component={SearchResult}/>
-            {staticPageRoutes}
-            <Route path="/catalog/:category" component={Catalog}/>
-          <Route path="/catalog/:category" component={Catalog}/>
-            <Route path="/products/:itemNumber" component={ProductPage}/>
-            <PrivateRoute isAuthenticated={isAuthenticated} path="/checkout" component={Checkout}/>
-            <PrivateRoute isAuthenticated={isAuthenticated} path="/member" component={Member}/>
-            <Route path="/cart" component={Cart}/>
-            <Route path="/auth" component={Authentication}/>
-            <Route exact path="/" component={Home}/>
-            <Route path="*" component={Page404} />
-        </Switch>
-    );
+  let staticPageRoutes;
+
+  if (staticPages.length) {
+    staticPageRoutes = staticPages.map(page => {
+      return <Route
+        key={page._id}
+        path={page.url}
+        render={(renderProps) =>
+          <StaticPage
+            title={page.title}
+            htmlContent={page.htmlContent}
+            {...renderProps} />} />;
+    });
+  }
+
+  return (
+    <Switch>
+      <Route path='/products/search' component={SearchResult} />
+      {staticPageRoutes}
+      <Route path='/catalog/:category' component={Catalog} />
+      <Route path='/catalog/:category' component={Catalog} />
+      <Route path='/products/:itemNumber' component={Product} />
+      <PrivateRoute isAuthenticated={isAuthenticated} path='/checkout' component={Checkout} />
+      <PrivateRoute isAuthenticated={isAuthenticated} path='/member' component={Member} />
+      <Route path='/cart' component={Cart} />
+      <Route path='/auth' component={Authentication} />
+      <Route exact path='/' component={Home} />
+      <Route path='*' component={Page404} />
+    </Switch>
+  );
 
 }
 
--- client/src/view/routes/AppRoutes.test.jsx
+++ client/src/view/routes/AppRoutes.test.jsx
@@ -1,18 +1,18 @@
-import {createBrowserHistory} from "history";
-import {render} from "@testing-library/react";
-import {Router} from "react-router-dom";
-import AppRoutes from "./AppRoutes";
+import { createBrowserHistory } from 'history';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
 import renderWithProjectProviders from '../../utils/renderWithProjectProviders';
 import store from '../../redux/store/store';
 
-jest.mock("../pages/Authentication/Authentication.jsx", () => () => <p>Authentication</p>);
-jest.mock("../pages/Cart/Cart.jsx", () => () => <p>Cart</p>);
-jest.mock("../pages/Catalog/Catalog.jsx", () => () => <p>Catalog</p>);
-jest.mock("../pages/Checkout/Checkout.jsx", () => () => <p>Checkout</p>);
-jest.mock("../pages/Home/Home.jsx", () => () => <p>Home</p>);
-jest.mock("../pages/Member/Member.jsx", () => () => <p>Member</p>);
-jest.mock("../pages/Page404/Page404.jsx", () => () => <p>404</p>);
-jest.mock("../pages/Product/Product.jsx", () => () => <p>Product</p>);
+jest.mock('../pages/Authentication/Authentication.jsx', () => () => <p>Authentication</p>);
+jest.mock('../pages/Cart/Cart.jsx', () => () => <p>Cart</p>);
+jest.mock('../pages/Catalog/Catalog.jsx', () => () => <p>Catalog</p>);
+jest.mock('../pages/Checkout/Checkout.jsx', () => () => <p>Checkout</p>);
+jest.mock('../pages/Home/Home.jsx', () => () => <p>Home</p>);
+jest.mock('../pages/Member/Member.jsx', () => () => <p>Member</p>);
+jest.mock('../pages/Page404/Page404.jsx', () => () => <p>404</p>);
+jest.mock('../pages/Product/Product.jsx', () => () => <p>Product</p>);
 
 
 /**
@@ -23,143 +23,141 @@ jest.mock("../pages/Product/Product.jsx", () => () => <p>Product</p>);
  * that includes test, Test, TEST ... content
  *
  * */
-describe("AppRoutes", () => {
-    const history = createBrowserHistory();
+describe('AppRoutes', () => {
+  const history = createBrowserHistory();
 
-    beforeEach(() => {
-        history.push("/");
-        sessionStorage.clear()
-    })
+  beforeEach(() => {
+    history.push('/');
+    sessionStorage.clear();
+  });
 
-    test("if it renders home page", () => {
-        const {getByText} = renderWithProjectProviders(
-          <AppRoutes/>, store, history
-        );
+  test('if it renders home page', () => {
+    const { getByText } = renderWithProjectProviders(
+      <AppRoutes />, store, history,
+    );
 
-        expect(getByText(/home/i)).toBeInTheDocument();
-    });
+    expect(getByText(/home/i)).toBeInTheDocument();
+  });
 
-    test("if it renders authentication page when pathname is '/auth/login' ", () => {
-        const {queryByText} = renderWithProjectProviders(
-          <AppRoutes/>, store, history
-        );
+  test('if it renders authentication page when pathname is \'/auth/login\' ', () => {
+    const { queryByText } = renderWithProjectProviders(
+      <AppRoutes />, store, history,
+    );
 
-        expect(queryByText(/authentication/i)).toBeNull();
-        history.push("/auth/login");
-        expect(queryByText(/authentication/i)).toBeInTheDocument();
-    });
+    expect(queryByText(/authentication/i)).toBeNull();
+    history.push('/auth/login');
+    expect(queryByText(/authentication/i)).toBeInTheDocument();
+  });
 
-    test("if it renders authentication page when pathname is '/auth/register' ", () => {
-        const {queryByText} = renderWithProjectProviders(
-          <AppRoutes/>, store, history
-        );
+  test('if it renders authentication page when pathname is \'/auth/register\' ', () => {
+    const { queryByText } = renderWithProjectProviders(
+      <AppRoutes />, store, history,
+    );
 
-        expect(queryByText(/authentication/i)).toBeNull();
-        history.push("/auth/register");
-        expect(queryByText(/authentication/i)).toBeInTheDocument();
-    });
+    expect(queryByText(/authentication/i)).toBeNull();
+    history.push('/auth/register');
+    expect(queryByText(/authentication/i)).toBeInTheDocument();
+  });
 
-    test("if it renders cart page ", () => {
-        const {queryByText} = renderWithProjectProviders(
-          <AppRoutes/>, store, history
-        );
+  test('if it renders cart page ', () => {
+    const { queryByText } = renderWithProjectProviders(
+      <AppRoutes />, store, history,
+    );
 
-        expect(queryByText(/cart/i)).toBeNull();
-        history.push("/cart");
-        expect(queryByText(/cart/i)).toBeInTheDocument();
-    });
+    expect(queryByText(/cart/i)).toBeNull();
+    history.push('/cart');
+    expect(queryByText(/cart/i)).toBeInTheDocument();
+  });
 
-    test("if it renders catalog page ", () => {
-        const {queryByText} = renderWithProjectProviders(
-          <AppRoutes/>, store, history
-        );
+  test('if it renders catalog page ', () => {
+    const { queryByText } = renderWithProjectProviders(
+      <AppRoutes />, store, history,
+    );
 
-        expect(queryByText(/catalog/i)).toBeNull();
-        history.push("/catalog/men");
-        expect(queryByText(/catalog/i)).toBeInTheDocument();
-    });
+    expect(queryByText(/catalog/i)).toBeNull();
+    history.push('/catalog/men');
+    expect(queryByText(/catalog/i)).toBeInTheDocument();
+  });
 
-    // test("if it renders product page", () => {
-    //     const {debug, queryByText} = render(
-    //         <Router history={history}>
-    //             <AppRoutes/>
-    //         </Router>
-    //     );
-    //
-    //     expect(queryByText(/product/i)).toBeNull();
-    //     history.push("/catalog/12498");
-    //     expect(queryByText(/product/i)).toBeInTheDocument();
-    // });
+  test('if it renders product page', () => {
+    const { queryByText } = renderWithProjectProviders(
+      <AppRoutes />,store, history
+    );
 
-    test("if it doesn't render checkout page without token", () => {
-        const {queryByText} = renderWithProjectProviders(
-          <AppRoutes/>, store, history
-        );
+    expect(queryByText(/product/i)).toBeNull();
+    history.push('/products/12498');
+    expect(queryByText(/product/i)).toBeInTheDocument();
+  });
 
-        expect(sessionStorage.getItem("token")).toBeNull();
-        expect(queryByText(/authentication/i)).toBeNull();
-        expect(queryByText(/checkout/i)).toBeNull();
-        history.push("/checkout");
-        expect(history.location.pathname).toMatch(/login/);
-        expect(queryByText(/checkout/i)).toBeNull();
-        expect(queryByText(/authentication/i)).toBeInTheDocument();
-    });
+  test('if it doesn\'t render checkout page without token', () => {
+    const { queryByText } = renderWithProjectProviders(
+      <AppRoutes />, store, history,
+    );
 
-    test("if it renders checkout page with token", () => {
-        const tokenValue = "test"
-        sessionStorage.setItem("token", tokenValue);
+    expect(sessionStorage.getItem('token')).toBeNull();
+    expect(queryByText(/authentication/i)).toBeNull();
+    expect(queryByText(/checkout/i)).toBeNull();
+    history.push('/checkout');
+    expect(history.location.pathname).toMatch(/login/);
+    expect(queryByText(/checkout/i)).toBeNull();
+    expect(queryByText(/authentication/i)).toBeInTheDocument();
+  });
 
-        const {queryByText} = renderWithProjectProviders(
-          <AppRoutes/>, store, history
-        );
+  test('if it renders checkout page with token', () => {
+    const tokenValue = 'test';
+    sessionStorage.setItem('token', tokenValue);
 
-        expect(sessionStorage.getItem("token")).toMatch(tokenValue);
-        expect(queryByText(/authentication/i)).toBeNull();
-        expect(queryByText(/checkout/i)).toBeNull();
-        history.push("/checkout");
-        expect(history.location.pathname).toMatch(/checkout/);
-        expect(queryByText(/checkout/i)).toBeInTheDocument();
-        expect(queryByText(/authentication/i)).toBeNull();
-    });
+    const { queryByText } = renderWithProjectProviders(
+      <AppRoutes />, store, history,
+    );
 
-    test("If it doesn't render member page without token", () => {
-        const {queryByText} = renderWithProjectProviders(
-          <AppRoutes/>, store, history
-        );
+    expect(sessionStorage.getItem('token')).toMatch(tokenValue);
+    expect(queryByText(/authentication/i)).toBeNull();
+    expect(queryByText(/checkout/i)).toBeNull();
+    history.push('/checkout');
+    expect(history.location.pathname).toMatch(/checkout/);
+    expect(queryByText(/checkout/i)).toBeInTheDocument();
+    expect(queryByText(/authentication/i)).toBeNull();
+  });
 
-        expect(sessionStorage.getItem("token")).toBeNull();
-        expect(queryByText(/authentication/i)).toBeNull();
-        expect(queryByText(/member/i)).toBeNull();
-        history.push("/member");
-        expect(history.location.pathname).toMatch(/login/);
-        expect(queryByText(/member/i)).toBeNull();
-        expect(queryByText(/authentication/i)).toBeInTheDocument();
-    });
+  test('If it doesn\'t render member page without token', () => {
+    const { queryByText } = renderWithProjectProviders(
+      <AppRoutes />, store, history,
+    );
 
-    test("If it renders member page with token", () => {
-        const tokenValue = "test"
-        sessionStorage.setItem("token", tokenValue);
+    expect(sessionStorage.getItem('token')).toBeNull();
+    expect(queryByText(/authentication/i)).toBeNull();
+    expect(queryByText(/member/i)).toBeNull();
+    history.push('/member');
+    expect(history.location.pathname).toMatch(/login/);
+    expect(queryByText(/member/i)).toBeNull();
+    expect(queryByText(/authentication/i)).toBeInTheDocument();
+  });
 
-        const {queryByText} = renderWithProjectProviders(
-          <AppRoutes/>, store, history
-        );
+  test('If it renders member page with token', () => {
+    const tokenValue = 'test';
+    sessionStorage.setItem('token', tokenValue);
 
-        expect(sessionStorage.getItem("token")).toMatch(tokenValue);
-        expect(queryByText(/authentication/i)).toBeNull();
-        expect(queryByText(/member/i)).toBeNull();
-        history.push("/member");
-        expect(history.location.pathname).toMatch(/member/);
-        expect(queryByText(/member/i)).toBeInTheDocument();
-        expect(queryByText(/authentication/i)).toBeNull();
-    });
+    const { queryByText } = renderWithProjectProviders(
+      <AppRoutes />, store, history,
+    );
 
-    test("if it renders Page404 when the pathname doesn't match any Route", () => {
-        const {queryByText} = renderWithProjectProviders(
-          <AppRoutes/>, store, history
-        );
+    expect(sessionStorage.getItem('token')).toMatch(tokenValue);
+    expect(queryByText(/authentication/i)).toBeNull();
+    expect(queryByText(/member/i)).toBeNull();
+    history.push('/member');
+    expect(history.location.pathname).toMatch(/member/);
+    expect(queryByText(/member/i)).toBeInTheDocument();
+    expect(queryByText(/authentication/i)).toBeNull();
+  });
 
-        expect(queryByText(/404/i)).toBeNull();
-        history.push("/incorrect-route-at-all");
-        expect(queryByText(/404/i)).toBeInTheDocument();
-    });
-})
+  test('if it renders Page404 when the pathname doesn\'t match any Route', () => {
+    const { queryByText } = renderWithProjectProviders(
+      <AppRoutes />, store, history,
+    );
+
+    expect(queryByText(/404/i)).toBeNull();
+    history.push('/incorrect-route-at-all');
+    expect(queryByText(/404/i)).toBeInTheDocument();
+  });
+});
