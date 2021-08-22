88c1dff8ef336b24d770d590b7211cfc4a627db0
Darya Kozir <darya.kozir@gmail.com>
2021-08-22 09:18:35 +0300

    updates_image

--- client/src/view/components/Product/Sections/ImageMagnify.jsx
+++ client/src/view/components/Product/Sections/ImageMagnify.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactImageMagnify from "react-image-magnify";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -9,22 +8,7 @@ export default function ImageMagnify(props) {
     const renderSlides = () =>
         props.detail.imageUrls.map(num => (
             <div>
-                <ReactImageMagnify
-                    {...{
-                        smallImage: {
-                            isFluidWidth: false,
-                            src: num,
-                            width: 600,
-                            height: 500,
-                            // sizes: '(max-width: 480px) 100vw, (max-width: 1200px) 30vw, 360px'
-                        },
-                        largeImage: {
-                            src: num,
-                            width: 800,
-                            height: 800
-                        }
-                    }}
-                />
+                <img src={num} alt=""/>
             </div>
         ));
 
