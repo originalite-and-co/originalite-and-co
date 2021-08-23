a6200ebc4b7e3f52bca7ff51510b6ed54d20940c
drewmeltpool <drewmelt132@gmail.com>
2021-08-23 20:48:45 +0300

    test

--- client/src/view/App.jsx
+++ client/src/view/App.jsx
@@ -6,10 +6,7 @@ import { useTheme } from '@material-ui/styles';
 import CustomThemeProvider from './HOC/CustomThemeProvider/CustomThemeProvider';
 
 function App() {
-  return (
-    <CustomThemeProvider>
-      <AppRoutes />
-    </CustomThemeProvider>
+  return ( <CustomThemeProvider><AppRoutes />CustomThemeProvider>
   );
 }
 
