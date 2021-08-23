cc81a214e2deb9fe3c436e69a3c7982a08bee012
doichev-kostia <kostynd2004@gmail.com>
2021-08-23 21:17:34 +0300

    Deleted react-slick

--- client/src/view/components/Product/Sections/ImageMagnify.jsx
+++ client/src/view/components/Product/Sections/ImageMagnify.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
 export default function ImageMagnify(props) {
   const renderSlides = () =>
