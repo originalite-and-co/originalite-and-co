01e4e870624e471ad44d0c42bfdf0aba2fe63490
doichev-kostia <kostynd2004@gmail.com>
2021-08-23 20:02:17 +0300

    created redux actions and types for cart

--- client/src/redux/features/cart/operations.js
+++ client/src/redux/features/cart/operations.js
@@ -1,4 +1,34 @@
+import {cartRequests} from '../../../api/server';
+import actions from './actions';
 
-const operations = {};
+const getCart = () => async (dispatch) => {
+  try{
+    const response = await cartRequests.retrieveCart();
+    const data = response.products.map(({ cartQuantity, product }) => ({
+      cartQuantity,
+      _id: product._id,
+    }));
+    dispatch(actions.getCart(data));
+  }catch (error) {
+    console.error(error)
+  }
+};
+
+const addProductToCart = (id) => async (dispatch) => {
+  try{
+
+  } catch (error) {
+
+  }
+};
+
+const operations = {
+  getCart,
+
+};
+
+function getCartProducts (cart) {
+
+}
 
 export default operations;
