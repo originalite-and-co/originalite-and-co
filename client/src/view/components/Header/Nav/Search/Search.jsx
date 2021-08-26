22c821ca23fbad3726076def1369e4c939a5f352
Антон Молчанов <antonmolchanov97@gmail.com>
2021-08-26 16:14:56 +0300

    слил с девелопом

--- client/src/view/components/Header/Nav/Search/Search.jsx
+++ client/src/view/components/Header/Nav/Search/Search.jsx
@@ -1,179 +1,194 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import useAsyncError from '../../../../hooks/useAsyncError';
 import useWindowSize from '../../../../hooks/useWindowSize';
 import constants from '../../../../constants';
 
-import {useDispatch, useSelector} from 'react-redux';
-import {searchResultActions} from '../../../../../redux/features/searchResult';
-import {isAnyDropdownOpenActions, isAnyDropdownOpenSelectors} from '../../../../../redux/features/dropdown';
+import { useDispatch, useSelector } from 'react-redux';
+import { searchResultActions } from '../../../../../redux/features/searchResult';
+import {
+  isAnyDropdownOpenActions,
+  isAnyDropdownOpenSelectors
+} from '../../../../../redux/features/dropdown';
 
-import {useHistory} from 'react-router-dom';
-import {productRequests} from '../../../../../api/server/index';
+import { useHistory } from 'react-router-dom';
+import { productRequests } from '../../../../../api/server/index';
 
 import Toast from '../../../Toast/Toast';
 import Dropdown from '../../../Dropdown/Dropdown';
 import Button from '../../../Button/Button';
-import {Box, TextField} from '@material-ui/core';
+import { Box, TextField } from '@material-ui/core';
 
 import search from '../icons/search.svg';
-import {makeStyles} from "@material-ui/styles";
-import {generateStyles} from './SearchStyles'
+import { makeStyles } from '@material-ui/styles';
+import { generateStyles } from './SearchStyles';
 
-import {Formik} from 'formik';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
 
 Search.propTypes = {};
 
 function Search(props) {
-    const useStyles = makeStyles(generateStyles);
-    const classes = useStyles()
+  const useStyles = makeStyles(generateStyles);
+  const classes = useStyles();
 
-    const [searchResult, setSearchResult] = useState([]);
-    const [isLoaded, setLoaded] = useState(false);
-    const [isDropdownActive, setActiveDropdown] = useState(false);
-    const [isDesktop, setIsDesktop] = useState();
+  const [searchResult, setSearchResult] = useState([]);
+  const [isLoaded, setLoaded] = useState(false);
+  const [isDropdownActive, setActiveDropdown] = useState(false);
+  const [isDesktop, setIsDesktop] = useState();
 
-    const isAnyDropdownOpen = useSelector(isAnyDropdownOpenSelectors.getIsAnyDropdownOpen);
-    const dispatch = useDispatch();
+  const isAnyDropdownOpen = useSelector(
+    isAnyDropdownOpenSelectors.getIsAnyDropdownOpen
+  );
+  const dispatch = useDispatch();
 
-    const throwAsyncError = useAsyncError();
-    const history = useHistory();
+  const throwAsyncError = useAsyncError();
+  const history = useHistory();
 
-    const {width} = useWindowSize();
+  const { width } = useWindowSize();
 
-    useEffect(() => {
-        setIsDesktop(width >= constants.WINDOW_DESKTOP_SIZE);
-    }, [width]);
+  useEffect(() => {
+    setIsDesktop(width >= constants.WINDOW_DESKTOP_SIZE);
+  }, [width]);
 
-    useEffect(() => {
-        if (!isAnyDropdownOpen) {
-            setActiveDropdown(false);
-        }
-    }, [isAnyDropdownOpen, isDropdownActive]);
+  useEffect(() => {
+    if (!isAnyDropdownOpen) {
+      setActiveDropdown(false);
+    }
+  }, [isAnyDropdownOpen, isDropdownActive]);
 
-    const handleIconClick = (event) => {
-        if (isDropdownActive) {
-            dispatch(isAnyDropdownOpenActions.closedDropdown());
-            setActiveDropdown(false);
-        } else {
-            //close all dropdowns that are active
-            dispatch(isAnyDropdownOpenActions.closedDropdown());
+  const handleIconClick = (event) => {
+    if (isDropdownActive) {
+      dispatch(isAnyDropdownOpenActions.closedDropdown());
+      setActiveDropdown(false);
+    } else {
+      //close all dropdowns that are active
+      dispatch(isAnyDropdownOpenActions.closedDropdown());
 
-            /**
-             * These setTimeouts are important for functionality,
-             * as they are asynchronous and somehow guarantee that the code
-             * in their callback will be executed only
-             * when call stack in event loop is empty. This means that
-             * all setState and useEffect callbacks will be executed properly
-             */
-            setTimeout(() => {
-                dispatch(isAnyDropdownOpenActions.openedDropdown());
-            }, 0);
+      /**
+       * These setTimeouts are important for functionality,
+       * as they are asynchronous and somehow guarantee that the code
+       * in their callback will be executed only
+       * when call stack in event loop is empty. This means that
+       * all setState and useEffect callbacks will be executed properly
+       */
+      setTimeout(() => {
+        dispatch(isAnyDropdownOpenActions.openedDropdown());
+      }, 0);
 
-            setTimeout(() => {
-                setActiveDropdown(true);
-            });
-        }
-    };
+      setTimeout(() => {
+        setActiveDropdown(true);
+      });
+    }
+  };
 
   const handleSubmit = (values, actions) => {
     setLoaded(false);
-    productRequests.searchForProduct(values.search)
-      .then(
-        data => {
-          setSearchResult(data);
-          setLoaded(true);
-          const query = values.search.split(' ').join('-')
-          if (data.length) {
-            dispatch(searchResultActions.setSearchResult(data));
-            history.push(`/products/search?query=${query}`);
-            setActiveDropdown(false);
-            dispatch(isAnyDropdownOpenActions.closedDropdown());
-          }
-        },
-        error => throwAsyncError(error),
-      );
+    productRequests.searchForProduct(values.search).then(
+      (data) => {
+        setSearchResult(data);
+        setLoaded(true);
+        const query = values.search.split(' ').join('-');
+        if (data.length) {
+          dispatch(searchResultActions.setSearchResult(data));
+          history.push(`/products/search?query=${query}`);
+          setActiveDropdown(false);
+          dispatch(isAnyDropdownOpenActions.closedDropdown());
+        }
+      },
+      (error) => throwAsyncError(error)
+    );
   };
 
+  const informationToast = useMemo(
+    () => (
+      <Toast
+        variant="filled"
+        className={classes.informationToast}
+        message="No items have been found "
+      />
+    ),
+    []
+  );
 
-  const informationToast = useMemo(() => <Toast
-    variant="filled"
-    className={styles.informationToast}
-    message='No items have been found '
-  />, []);
+  const dropdownContent = (
+    <>
+      <Formik
+        initialValues={{
+          search: ''
+        }}
+        validationSchema={Yup.object().shape({
+          search: Yup.string()
+            .min(2, "It's required to type at least two characters")
+            .required('Fill in this field, please')
+        })}
+        onSubmit={handleSubmit}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched
+        }) => {
+          return (
+            <form onSubmit={handleSubmit} className={classes.form}>
+              <Box className={`${classes.textFieldWrapper} wrapper`}>
+                <TextField
+                  color="primary"
+                  value={values.search}
+                  fullWidth
+                  label="Search for item"
+                  inputProps={{
+                    onChange: handleChange,
+                    onBlur: handleBlur,
+                    name: 'search',
+                    className: classes.input
+                  }}
+                />
 
-    const dropdownContent = (
-        <>
-            <Formik
-                initialValues={{
-                    search: '',
-                }}
-                validationSchema={Yup.object().shape({
-                    search: Yup
-                        .string()
-                        .min(2, 'It\'s required to type at least two characters')
-                        .required('Fill in this field, please'),
+                {errors.search && touched.search && (
+                  <p className={classes.error}>{errors.search}</p>
+                )}
+              </Box>
+              <Box className={classes.btnWrapper}>
+                <Button
+                  text="search"
+                  type="submit"
+                  backgroundColor="#000000"
+                  color="#FFFFFF"
+                />
+              </Box>
+            </form>
+          );
+        }}
+      </Formik>
+      {isLoaded && !searchResult.length && informationToast}
+    </>
+  );
 
-                })}
-                onSubmit={handleSubmit}
-            >
-                {(
-                    {
-                        handleChange,
-                        handleBlur,
-                        handleSubmit,
-                        values,
-                        errors,
-                        touched,
-                    }) => {
-                    return (
-                        <form onSubmit={handleSubmit} className={classes.form}>
-
-                            <Box className={`${classes.textFieldWrapper} wrapper`}>
-                                <TextField
-                                    color='primary'
-                                    value={values.search}
-                                    fullWidth
-                                    label='Search for item'
-                                    inputProps={{
-                                        onChange: handleChange,
-                                        onBlur: handleBlur,
-                                        name: 'search',
-                                        className: classes.input,
-                                    }}
-                                />
-
-                                {errors.search && touched.search && <p className={classes.error}>{errors.search}</p>}
-                            </Box>
-                            <Box className={classes.btnWrapper}>
-                                <Button text='search' type='submit' backgroundColor='#000000' color='#FFFFFF'/>
-                            </Box>
-                        </form>
-                    );
-                }}
-            </Formik>
-            {isLoaded && !searchResult.length && informationToast}
-        </>
-    );
-
-    return (
-        <>
-            <Box onClick={handleIconClick} className={classes.imageWrapper} data-testid='nav-item-search'>
-                <img className={classes.icon} src={search} alt='search icon'/>
-                {isDesktop && <p>Search</p>}
-            </Box>
-            <Dropdown
-                lockBodyScrolling
-                classNames={{
-                    closed: classes.dropdown,
-                    active: classes.dropdownActive,
-                }}
-                isActive={isDropdownActive}
-                children={dropdownContent}
-            />
-        </>
-    );
+  return (
+    <>
+      <Box
+        onClick={handleIconClick}
+        className={classes.imageWrapper}
+        data-testid="nav-item-search"
+      >
+        <img className={classes.icon} src={search} alt="search icon" />
+        {isDesktop && <p>Search</p>}
+      </Box>
+      <Dropdown
+        lockBodyScrolling
+        classNames={{
+          closed: classes.dropdown,
+          active: classes.dropdownActive
+        }}
+        isActive={isDropdownActive}
+        children={dropdownContent}
+      />
+    </>
+  );
 }
 
 export default Search;
--- client/src/view/pages/Authentication/AuthenticationContent/LoginPage/LoginPage.jsx
+++ client/src/view/pages/Authentication/AuthenticationContent/LoginPage/LoginPage.jsx
@@ -1,39 +1,37 @@
-import React, {useState} from 'react';
-import Styles from "../Authentication.module.scss";
-import Box from "@material-ui/core/Box";
-import TextField from "@material-ui/core/TextField";
-import Button from "../../../../components/Button/Button";
-import {makeStyles} from "@material-ui/core/styles";
-import {Form, Formik, Field, va, ErrorMessage} from 'formik'
-import {object, string} from "yup";
-import {FormGroup, Typography} from "@material-ui/core";
-import {customerRequests} from "../../../../../api/server";
-import useAsyncError from "../../../../hooks/useAsyncError";
-import {useHistory} from "react-router-dom";
-import Toast from "../../../../components/Toast/Toast";
+import React, { useState } from 'react';
+import Styles from '../Authentication.module.scss';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Button from '../../../../components/Button/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { Form, Formik, Field, ErrorMessage } from 'formik';
+import { FormGroup, Typography } from '@material-ui/core';
+import { customerRequests } from '../../../../../api/server';
+import useAsyncError from '../../../../hooks/useAsyncError';
+import { useHistory } from 'react-router-dom';
+import Toast from '../../../../components/Toast/Toast';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
-import {useDispatch} from 'react-redux';
-import {
-    authorizeOperations,
-} from '../../../../../redux/features/authorization';
+import { useDispatch } from 'react-redux';
+import { authorizeOperations } from '../../../../../redux/features/authorization';
+import * as yup from 'yup';
 
 const useStyles = makeStyles({
-    textField: {
-        borderBottom: '1px solid white',
-        color: '#FFFFFF !important',
-    },
-    radio: {
-        color: '#FFFFFF !important',
-    },
-    visibilityBtn: {
-        color: '#FFFFFF !important',
-        position: 'absolute',
-        top: '30%',
-        right: 0,
-    },
+  textField: {
+    borderBottom: '1px solid white',
+    color: '#FFFFFF !important'
+  },
+  radio: {
+    color: '#FFFFFF !important'
+  },
+  visibilityBtn: {
+    color: '#FFFFFF !important',
+    position: 'absolute',
+    top: '30%',
+    right: 0
+  }
 });
 
 const initialValues = {
@@ -47,14 +45,14 @@ const validationSchema = yup.object().shape({
 });
 
 function LoginPage() {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const classes = useStyles()
-    const throwAsyncError = useAsyncError()
-    const history = useHistory()
-    const [loggedIn, setLoggedIn] = useState(false)
-    const [typePassword, setTypePassword] = useState(true)
-    const [checked, setChecked] = React.useState(false);
+  const classes = useStyles();
+  const throwAsyncError = useAsyncError();
+  const history = useHistory();
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [typePassword, setTypePassword] = useState(true);
+  const [checked, setChecked] = React.useState(false);
 
   const keepMeSignedIn = checked ? (
     <RadioButtonCheckedIcon fontSize="small" className={classes.radio} />
@@ -64,18 +62,18 @@ function LoginPage() {
 
   const { authorizeUser } = authorizeOperations;
 
-    const forwardIfAuthorized = async () => {
-        dispatch(authorizeUser());
-        if (sessionStorage.getItem('token') || localStorage.getItem('token')) {
-            setLoggedIn(true)
-            setTimeout(() => {
-                history.push('/');
-            }, 1500);
-        }
-    };
-    const handleChange = () => {
-        setChecked(!checked);
-    };
+  const forwardIfAuthorized = async () => {
+    dispatch(authorizeUser());
+    if (sessionStorage.getItem('token') || localStorage.getItem('token')) {
+      setLoggedIn(true);
+      setTimeout(() => {
+        history.push('/');
+      }, 1500);
+    }
+  };
+  const handleChange = () => {
+    setChecked(!checked);
+  };
 
   const handlePasswordVisibilityClick = () => {
     setTypePassword(!typePassword);
@@ -95,94 +93,99 @@ function LoginPage() {
     />
   );
 
-    return (
-        <>
-            {loggedIn && <Toast severity="success" variant="filled" message="Welcome to Originalite, fashionista"/>}
-            <Box className={Styles.logInPageWrapper} data-testid="login-page">
-                <p className={Styles.text}>
-                    Please enter your account details to log in
-                </p>
-                <Formik
-                    validationSchema={validationSchema}
-                    initialValues={initialValues}
-                    onSubmit={(values) => {
-                        customerRequests
-                            .logIn(values, checked)
-                            .then(forwardIfAuthorized)
-                            .catch((error) => throwAsyncError(error));
-                    }}
-                >
-                    {({values, errors, touched, isSubmitting, isValidating}) => (
-                        <Form>
-                            <Box className={Styles.loginGroup}>
-                                <FormGroup data-testid="loginOrEmail">
-                                    <Field
-                                        name="loginOrEmail"
-                                        as={TextField}
-                                        data-testid="email-input"
-                                        required
-                                        fullWidth
-                                        label="E-mail"
-                                        className={classes.textField}
-                                        InputLabelProps={{className: Styles.textFieldInput}}
-                                        inputProps={{className: Styles.textFieldInput}}
-                                    />
-                                    <ErrorMessage
-                                        name="loginOrEmail"
-                                        render={(msg) => (
-                                            <Typography color="error">{msg}</Typography>
-                                        )}
-                                    />
-                                </FormGroup>
-                                <FormGroup data-testid="password">
-                                    <Box style={{position: 'relative'}}>
-                                        <Field
-                                            name="password"
-                                            as={TextField}
-                                            type={typePassword ? 'password' : 'text'}
-                                            required
-                                            fullWidth
-                                            label="Password"
-                                            className={classes.textField}
-                                            InputLabelProps={{className: Styles.textFieldInput}}
-                                            inputProps={{className: Styles.textFieldInput}}
-                                        />
-                                        {passwordVisibility}
-                                    </Box>
-
-                                    <ErrorMessage
-                                        name="password"
-                                        render={(msg) => (
-                                            <Typography color="error">{msg}</Typography>
-                                        )}
-                                    />
-                                </FormGroup>
-                                <Box
-                                    onClick={handleChange}
-                                    className={Styles.radio}
-                                    data-testid="radio"
-                                >
-                                    {keepMeSignedIn}
-                                    <p className={Styles.radioText}>Keep me signed in</p>
-                                </Box>
-                            </Box>
-                            <Box className={Styles.loginBtn}>
-                                <Button
-                                    disabled={isSubmitting || isValidating}
-                                    type="submit"
-                                    text="LOG IN"
-                                    backgroundColor="#E5E5E5"
-                                    color="#000000"
-                                    onClick={() => {
-                                    }}
-                                />
-                            </Box>
-                        </Form>
+  return (
+    <>
+      {loggedIn && (
+        <Toast
+          severity="success"
+          variant="filled"
+          message="Welcome to Originalite, fashionista"
+        />
+      )}
+      <Box className={Styles.logInPageWrapper} data-testid="login-page">
+        <p className={Styles.text}>
+          Please enter your account details to log in
+        </p>
+        <Formik
+          validationSchema={validationSchema}
+          initialValues={initialValues}
+          onSubmit={(values) => {
+            customerRequests
+              .logIn(values, checked)
+              .then(forwardIfAuthorized)
+              .catch((error) => throwAsyncError(error));
+          }}
+        >
+          {({ values, errors, touched, isSubmitting, isValidating }) => (
+            <Form>
+              <Box className={Styles.loginGroup}>
+                <FormGroup data-testid="loginOrEmail">
+                  <Field
+                    name="loginOrEmail"
+                    as={TextField}
+                    data-testid="email-input"
+                    required
+                    fullWidth
+                    label="E-mail"
+                    className={classes.textField}
+                    InputLabelProps={{ className: Styles.textFieldInput }}
+                    inputProps={{ className: Styles.textFieldInput }}
+                  />
+                  <ErrorMessage
+                    name="loginOrEmail"
+                    render={(msg) => (
+                      <Typography color="error">{msg}</Typography>
                     )}
-                </Formik>
-            </Box>
-        </>
-    );
+                  />
+                </FormGroup>
+                <FormGroup data-testid="password">
+                  <Box style={{ position: 'relative' }}>
+                    <Field
+                      name="password"
+                      as={TextField}
+                      type={typePassword ? 'password' : 'text'}
+                      required
+                      fullWidth
+                      label="Password"
+                      className={classes.textField}
+                      InputLabelProps={{ className: Styles.textFieldInput }}
+                      inputProps={{ className: Styles.textFieldInput }}
+                    />
+                    {passwordVisibility}
+                  </Box>
+
+                  <ErrorMessage
+                    name="password"
+                    render={(msg) => (
+                      <Typography color="error">{msg}</Typography>
+                    )}
+                  />
+                </FormGroup>
+                <Box
+                  onClick={handleChange}
+                  className={Styles.radio}
+                  data-testid="radio"
+                >
+                  {keepMeSignedIn}
+                  <p className={Styles.radioText}>Keep me signed in</p>
+                </Box>
+              </Box>
+              <Box className={Styles.loginBtn}>
+                <Button
+                  disabled={isSubmitting || isValidating}
+                  type="submit"
+                  text="LOG IN"
+                  backgroundColor="#E5E5E5"
+                  color="#000000"
+                  onClick={() => {}}
+                />
+              </Box>
+            </Form>
+          )}
+        </Formik>
+      </Box>
+    </>
+  );
 }
 
 export default LoginPage;
