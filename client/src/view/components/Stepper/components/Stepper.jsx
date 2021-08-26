11e4f4db741a0c229db83fb7614e8e8ab98b1651
drewmeltpool <drewmelt132@gmail.com>
2021-08-27 00:19:58 +0300

    init stepper

--- client/src/view/components/Stepper/components/Stepper.jsx
+++ client/src/view/components/Stepper/components/Stepper.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Stepper from '@material-ui/core/Stepper';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%'
-  },
-  button: {
-    marginRight: theme.spacing(1)
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1)
-  }
-}));
 
 export default function HorizontalLinearStepper() {
-  const classes = useStyles();
-
+  
   return (
-    <div className={classes.root}>
-      <Stepper></Stepper>
+    <div>
+     kek
     </div>
   );
 }
