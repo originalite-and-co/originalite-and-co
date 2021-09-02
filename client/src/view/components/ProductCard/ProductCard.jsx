79e52434e0b4e09afb43f4ea4d88091bf9e5fc43
Kostia <72884674+doichev-kostia@users.noreply.github.com>
2021-09-02 20:01:17 +0300

    Added more cards in a row for different resolutions (#61)

--- client/src/view/components/ProductCard/ProductCard.jsx
+++ client/src/view/components/ProductCard/ProductCard.jsx
@@ -1,16 +1,14 @@
 import PropTypes from 'prop-types';
 import styles from './ProductCard.module.scss';
-import { Grid, Typography, Box } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import { Link, useRouteMatch } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import useWindowSize from '../../hooks/useWindowSize';
 import constants from '../../constants';
-import { useSelector } from 'react-redux';
-import { authorizationSelectors } from '../../../redux/features/authorization';
 
 ProductCard.propTypes = {
   product: PropTypes.object.isRequired,
-  size: PropTypes.number.isRequired,
+  size: PropTypes.number.isRequired
 };
 
 function ProductCard({ product, size }) {
@@ -24,46 +22,49 @@ function ProductCard({ product, size }) {
   }, [width]);
 
   const handleClick = () => {
-    const dataFromLocalStorage = JSON.parse(localStorage.getItem('recentlyViewed'));
+    const dataFromLocalStorage = JSON.parse(
+      localStorage.getItem('recentlyViewed')
+    );
     if (dataFromLocalStorage?.length === 10) {
-      dataFromLocalStorage.shift()
+      dataFromLocalStorage.shift();
     }
     let data = [product];
-    if(Array.isArray(dataFromLocalStorage)){
-      dataFromLocalStorage.some(item => item.itemNo === product.itemNo) ? data = [...dataFromLocalStorage] : data = [...dataFromLocalStorage, product]
+    if (Array.isArray(dataFromLocalStorage)) {
+      dataFromLocalStorage.some((item) => item.itemNo === product.itemNo)
+        ? (data = [...dataFromLocalStorage])
+        : (data = [...dataFromLocalStorage, product]);
     }
-    localStorage.setItem("recentlyViewed",JSON.stringify(data));
-  }
+    localStorage.setItem('recentlyViewed', JSON.stringify(data));
+  };
 
   return (
-      <Box
-          data-testid='product-card'
-          className={styles.productCard}
-          onClick={handleClick}
-      >
-        <Link to={`/products/${product.itemNo}`} className={styles.link}>
-          <div className={styles.productImage}>
-            <img src={product.imageUrls[0]} alt='products images' />
-          </div>
-          <Typography
-              color={path === '/' ? 'textPrimary' : 'textSecondary'}
-              component='p'
-              variant={isDesktop ? 'h6' : 'body2'}
-              className={styles.productCardTitle}
-          >
-            {product.name}
-          </Typography>
-          <Typography
-              component='p'
-              variant={isDesktop ? 'h6' : 'body2'}
-              className={styles.productCardPrice}
-          >
-            {`${product.currentPrice} $`}
-          </Typography>
-        </Link>
-      </Box>
+    <Box
+      data-testid="product-card"
+      className={styles.productCard}
+      onClick={handleClick}
+    >
+      <Link to={`/products/${product.itemNo}`} className={styles.link}>
+        <div className={styles.productImage}>
+          <img src={product.imageUrls[0]} alt="products images" />
+        </div>
+        <Typography
+          color={path === '/' ? 'textPrimary' : 'textSecondary'}
+          component="p"
+          variant={isDesktop ? 'h6' : 'body2'}
+          className={styles.productCardTitle}
+        >
+          {product.name}
+        </Typography>
+        <Typography
+          component="p"
+          variant={isDesktop ? 'h6' : 'body2'}
+          className={styles.productCardPrice}
+        >
+          ${product.currentPrice}
+        </Typography>
+      </Link>
+    </Box>
   );
 }
-
 
 export default ProductCard;
