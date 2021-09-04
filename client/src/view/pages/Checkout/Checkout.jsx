37aadbdc7b9d8df6b801605fb51e81dd16047cd7
drewmeltpool <drewmelt132@gmail.com>
2021-09-05 00:31:57 +0300

    merge: after merge into develop

--- client/src/view/pages/Checkout/Checkout.jsx
+++ client/src/view/pages/Checkout/Checkout.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import { Stepper, Step } from '../../components/Stepper';
 
 import { Box } from '@material-ui/core';
-import { Stepper, Step } from '../../components/Stepper';
 import { stepper, payment, userData, delivery } from './data';
 
 import { useSelector } from 'react-redux';
-import { cartSelectors } from '../../../redux/features/cart/index.js';
+import { cartSelectors } from '../../../redux/features/cart';
 
 import styles from './style';
 
+import useAsyncError from '../../hooks/useAsyncError';
+import { productRequests } from '../../../api/server';
+
 function Checkout() {
   const cart = useSelector(cartSelectors.getCart);
+  const [products, setProducts] = useState([]);
+  const throwAsyncError = useAsyncError();
+
+  console.log(products);
+
+  useEffect(() => {
+    if (!cart.length) return;
+
+    const catrItems = cart.map(({ itemNo }) => itemNo);
+    (async () => {
+      try {
+        const response = await productRequests.retrieveProductsByItemNumbers(
+          catrItems,
+        );
+        setProducts(response);
+      } catch (error) {
+        throwAsyncError(error);
+      }
+    })();
+  }, [cart, throwAsyncError]);
   const useStyle = styles();
 
   console.log(cart);
